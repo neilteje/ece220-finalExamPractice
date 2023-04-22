9e8e2f5f133f3565a9087d3933c2c56ff8233c07
Neil Teje <neilteje@gmail.com>
2023-04-22 00:08:18 -0500

    4/21 linked lists

--- linkedlist-practice/farey_seq.c
+++ linkedlist-practice/farey_seq.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "farey_seq.h"
+typedef struct node {
+    int numerator;
+    int denominator;
+    node *next;
+} node;
 
 int main()
 {
@@ -17,6 +22,33 @@ int main()
 node * farey_seq(int n)
 {
 	/*You code goes here*/
+    node *head = (node *)malloc(sizeof(node));
+    head->numerator = 0;
+    head->denominator = 1;
+    head->next = NULL;
+    node *second = (node *)malloc(sizeof(node));
+    second->numerator = 1;
+    second->denominator = 1;
+    second->next = NULL;
+    head->next = second;
+    for (int order = 2; order <= n; order++) {
+        node *current = head;
+        while (current->next != NULL) {
+            int a = current->numerator;
+            int b = current->denominator;
+            int c = current->next->numerator;
+            int d = current->next->denominator;
+            if (c + d <= order) {
+                node *new_node = (node *)malloc(sizeof(node));
+                new_node->numerator = a + c;
+                new_node->denominator = b + d;
+                new_node->next = current->next;
+                current->next = new_node;
+            }
+            current = current->next;
+        }
+    }
+    return head;
 }
 void print_list(node * head, int n)
 {
