0c7b2be87ad7d511383e89f30ae653d5250a1847
Neil Teje <neilteje@gmail.com>
2023-04-22 18:54:48 -0500

    4/22 2014final-problem1 changes

--- fall2014-final/problem1/linkedList.c
+++ fall2014-final/problem1/linkedList.c
@@ -4,14 +4,43 @@
 int DetectLoop(node *head)
 {
   //Write code here.
-    
+  if (head == NULL) {
+    return 0;
+  }
+  node* slow = head;
+  node* fast = head;
+  while (fast != NULL && fast->next != NULL) {
+    slow = slow->next;
+    fast = fast->next->next;
+    if (slow == fast) {
+      return 1;
+    }
+  }
+  return 0;
 }
 
 node *CopyList(node *head)
 {
-   //Insert Code here. You can change the return statement given.
-   
-   
-   
-   return NULL;
+  //Insert Code here. You can change the return statement given.
+  node* current = head;
+  node* newList = NULL;
+  node* tail = NULL;
+ 
+  while (current != NULL)
+  {
+    if (newList == NULL)
+    {
+      newList = (struct Node*)malloc(sizeof(node));
+      newList->value = current->value;
+      newList->next = NULL;
+      tail = newList;
+    } else {
+      tail->next = (struct Node*)malloc(sizeof(node));
+      tail = tail->next;
+      tail->value = current->value;
+      tail->next = NULL;
+    }
+    current = current->next;
+  }
+  return newList;
 }
