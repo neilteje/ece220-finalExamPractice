c38c34e1b1af175c32e9574b1807c931f18a2d35
Neil Teje <neilteje@gmail.com>
2023-04-24 07:37:02 -0500

    4/24 working left_most.asm

--- fall2012-final/problem1/left_most.asm
+++ fall2012-final/problem1/left_most.asm
@@ -34,23 +34,38 @@ RSTACK
 ; }
 LEFT_MOST
     ; TODO: Setup activation record
+    ADD R6, R6, #-3
+	STR R5, R6, #0
+	ADD R5, R6, #0
+    STR R7, R5, #1
 
     ; TODO: Check if root->left is NULL, if it is, branch to DONE
     ; if (!root->left)
+    LDR R0, R5, #3
+	LDR R1, R0, #0
+	BRz DONE
 
     ; TODO: Call left_most(root->left) and place the return value
     ; left_most(root->left) in the correct location in the
     ; activation record.
+    ADD R6, R6, #-1
+	STR R1, R6, #0
+	JSR LEFT_MOST
 
     BR TEARDOWN
 
 DONE
     ; TODO: return data: place the return value in the correct location
     ; in the activation record
+    LDR R2, R0, #2
+	STR R2, R5, #2
 
 TEARDOWN
 
     ; TODO: teardown activation record prior to RET
+    STR R7, R5, #1
+	STR R5, R5, #0
+	ADD R6, R6, #2
 
     RET
 
