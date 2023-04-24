b62b2928caa971aebc17f9f3f05c29331952bdec
Neil Teje <neilteje@gmail.com>
2023-04-24 07:42:41 -0500

    4/24 working sums.c

--- fall2014-final/problem3/sums.c
+++ fall2014-final/problem3/sums.c
@@ -30,10 +30,33 @@ int main(int argc, char * argv[])
 void findPathRecursive(node * root, int expectedSum, int currentSum, vector * path)
 {
     // YOUR CODE HERE
+    if (currentSum == expectedSum){ //to check if desired path length is reached 
+        if (!(root->left) && !(root->right)){ //check if it's a leaf node
+            int end = path->back;
+            for (int i = 0; i<=end; i++){
+                printf("%d ", path[i]);
+            }
+            printf("\n");
+            return;
+        }
+    }
+    if (!root){
+        return;
+    }
+    if (!isFull(path)){
+        currentSum += root->value;
+        pushBack(path, root->value);
+        findPathRecursive(root->left, expectedSum, currentSum, path);
+        findPathRecursive(root->right, expectedSum, currentSum, path);
+    }
 }
 
 // Finds and prints all paths within a tree that add to an expected sum
 void findPath(node * root, int expectedSum)
 {
     // YOUR CODE HERE
+    vector* myvector = (vector*)malloc(sizeof(vector));
+    vectorInit(myvector);
+    findPathRecursive(root, expectedSum, 0, myvector);
+    return;
 }
